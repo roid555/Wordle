e355c1943539387fe07445ed54072be790d6ae4f
Roi <roi8542@gmail.com>
2025-09-03 01:42:23 +0300

    adjust keyDown

--- src/assets/MyRow.jsx
+++ src/assets/MyRow.jsx
@@ -7,12 +7,31 @@ const MyRow = () => {
   const inputs = useRef([]);
 
   function handleChange(e, index) {
-    index < LETTER_IN_WORD - 1 ? inputs.current[index + 1].focus() : null;
+    if (e.target.value.length === 1) {
+      index < LETTER_IN_WORD - 1 ? inputs.current[index + 1].focus() : null;
+    }
+    if (e.key === "Backspace" && e.target.value.length === 0) {
+      index > 0 ? inputs.current[index - 1].focus() : null;
+    }
+  }
+
+  function handleSelect(e, index) {
+    while (index > 0 && inputs.current[index - 1].value.length === 0) {
+      inputs.current[index - 1].focus();
+      index--;
+    }
+    while (
+      index < LETTER_IN_WORD - 1 &&
+      inputs.current[index].value.length === 1
+    ) {
+      inputs.current[index + 1].focus();
+      index++;
+    }
   }
 
   return (
     <div className="row-container">
-      {[...Array(5)].map((_, index) => (
+      {[...Array(LETTER_IN_WORD)].map((_, index) => (
         <input
           className="letter-box"
           key={index}
@@ -20,6 +39,8 @@ const MyRow = () => {
           maxLength={1}
           ref={(e) => (inputs.current[index] = e)}
           onChange={(e) => handleChange(e, index)}
+          onSelect={(e) => handleSelect(e, index)}
+          onKeyDown={(e) => handleChange(e, index)}
         ></input>
       ))}
     </div>
